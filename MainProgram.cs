5e109da6971a8af781f6d193c3e91e13757ca79c
venkatesh179 <venkateshdyarangula@gmail.com>
2021-03-11 13:21:47 +0530

    UC2 merged

--- MainProgram.cs
+++ MainProgram.cs
@@ -6,22 +6,18 @@ namespace Gambler
     {
         static void Main(string[] args)
         {
-           //Console.WriteLine("HelloWorld");
-            
          UC1_Start var = new UC1_Start();
          int game = var.getEvery_Game();
          int day = var.getEvery_Day();
          int bet = var.win_Loose();
          if (bet == 0)
             {
-                //int day = var.every_Day;
                 Console.WriteLine("Loose the game");
                 day -= 1;
                 Console.WriteLine(day);
             }
             else if (bet == 1)
             {
-                //int day = var.every_Day;
                 Console.WriteLine("Won the Game");
                 day += 1;
                 Console.WriteLine(day);
