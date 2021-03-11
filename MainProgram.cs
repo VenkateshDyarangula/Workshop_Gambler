651a3a5c1381d6de64d685c5e81745950548ee92
venkatesh179 <venkateshdyarangula@gmail.com>
2021-03-11 13:32:19 +0530

    UC added

--- MainProgram.cs
+++ MainProgram.cs
@@ -6,24 +6,35 @@ namespace Gambler
     {
         static void Main(string[] args)
         {
-         UC1_Start var = new UC1_Start();
-         UC3_Resign uc3 = new UC3_Resign();
-         int game = var.getEvery_Game();
-         int day = var.getEvery_Day();
-         int bet = var.win_Loose();
-         if (bet == 0)
+            
+            UC4_After_20_Days uc4 = new UC4_After_20_Days();
+            UC3_Resign uc3 = new UC3_Resign();
+            //Uc1 Calling
+            Program p = new Program();
+            p.winorLoose();
+            uc3.resigned();
+            uc4.after20Days();
+        }
+        public void winorLoose()
+        {
+            UC1_Start var = new UC1_Start();
+            int game = var.getEvery_Game();
+            int day = var.getEvery_Day();
+            int bet = var.win_Loose();
+            Console.WriteLine(game);
+            Console.WriteLine(bet);
+            if (bet == 0)
             {
+                //int day = var.every_Day;
                 Console.WriteLine("Loose the game");
                 day -= 1;
-                Console.WriteLine(day);
             }
             else if (bet == 1)
             {
+                //int day = var.every_Day;
                 Console.WriteLine("Won the Game");
                 day += 1;
-                Console.WriteLine(day);
             }
-          uc3.resigned();
-   }
+        }
+    }
 }
-
--- Program.cs
+++ Program.cs
@@ -4,36 +4,37 @@ using System.Text;
 
 namespace Gambler
 {
-
-    class UC3_Resign 
-    {
-
-        public void resigned()
-        {
-            UC1_Start uc11 = new UC1_Start();
-            int stack = uc11.getEvery_Day();
-            int bet = uc11.win_Loose();
-            if (bet == 0)
-            {
-               Console.WriteLine("Loose the game");
-                stack = stack - 50;
-                if (stack == 50)
+     class UC4_After_20_Days
+    {  
+          public void after20Days()
+          { 
+            UC1_Start varr = new UC1_Start();
+            int bet = varr.win_Loose();
+            int stack = varr.getEvery_Day();
+            int total_Day = 20;
+            for (int i = 1; i <= total_Day; i++)
                 {
-                    Console.WriteLine("Resigned");
-                }
+                    if (bet == 0)
+                    {
+                       // Console.WriteLine("Loose the game");
+                        //stack = stack - 50;
+                        stack = i + stack;
+                        if (stack == 50)
+                        {
+                            Console.WriteLine("Resigned");
+                        }
 
-            }
-            else if (bet == 1)
-            {
-                Console.WriteLine("Won the Game");
-                stack = stack + 50;
-                if (stack == 150)
-                {
-                    Console.WriteLine("Resigned");
-                }
-            }
+                    }
+                    else if (bet == 1)
+                    {
+                        //Console.WriteLine("Won the Game");
+                        stack = stack + 50;
+                        if (stack == 150)
+                        {
+                            Console.WriteLine("Resigned");
+                        }
+                    }   
+             }            
         }
-    }  
-
+    }
 }
-
