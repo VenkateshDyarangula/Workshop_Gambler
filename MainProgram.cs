eba7ed94f81ec45e9b9bf605514debe173df9dfc
venkatesh179 <venkateshdyarangula@gmail.com>
2021-03-11 13:19:28 +0530

    UC2_Check Added

--- MainProgram.cs
+++ MainProgram.cs
@@ -17,12 +17,14 @@ namespace Gambler
                 //int day = var.every_Day;
                 Console.WriteLine("Loose the game");
                 day -= 1;
+                Console.WriteLine(day);
             }
             else if (bet == 1)
             {
                 //int day = var.every_Day;
                 Console.WriteLine("Won the Game");
                 day += 1;
+                Console.WriteLine(day);
             }
     
    }
