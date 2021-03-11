de773869d527f602d3ad96bb7eeb513e437a2634
venkatesh179 <venkateshdyarangula@gmail.com>
2021-03-11 13:02:55 +0530

    UC1 Added

--- MainProgram.cs
+++ MainProgram.cs
@@ -6,8 +6,25 @@ namespace Gambler
     {
         static void Main(string[] args)
         {
-           Console.WriteLine("HelloWorld");
+           //Console.WriteLine("HelloWorld");
             
-    }
+         UC1_Start var = new UC1_Start();
+         int game = var.getEvery_Game();
+         int day = var.getEvery_Day();
+         int bet = var.win_Loose();
+         if (bet == 0)
+            {
+                //int day = var.every_Day;
+                Console.WriteLine("Loose the game");
+                day -= 1;
+            }
+            else if (bet == 1)
+            {
+                //int day = var.every_Day;
+                Console.WriteLine("Won the Game");
+                day += 1;
+            }
+    
+   }
 }
 
